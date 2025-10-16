989f5454db6552a5b50ddfeb1272192a6813e4d1
SrFreitass <guilhermefreitasdonascimento67@gmail.com>
2025-10-16 12:08:02 -0400

    refactor: redirect url with datomic and shortened url page (hiccup)

--- src/url_shortener/core.clj
+++ src/url_shortener/core.clj
@@ -1,11 +1,14 @@
 (ns url-shortener.core
   (:gen-class)
   (:require [org.httpkit.server :as server]
-            [url-shortener.routes.routes :refer [app-routes]]))
+            [url-shortener.routes.routes :refer [app-routes]]
+            [url-shortener.db.db :as db]))
 
 (defn -main
   "Ponto de entrada da aplicação"
   []
-  (let [port 3001]
+  (db/create-db)
+  (db/create-schema)
+  (let [port 3002]
     (server/run-server #'app-routes {:port port})
     (println (str "Rodando o servidor em http://127.0.0.1:" port "/"))))
--- src/url_shortener/services/short_url.clj
+++ src/url_shortener/services/short_url.clj
@@ -7,14 +7,11 @@
   (apply str (repeatedly 10 #(rand-nth chars))))
 
 (defn short-url-service [url]
-  (let [url (db/get-url url)]
-    (if (not= url nil) url
-        (loop [id utils/gen-id]
-          (if (= (db/get-url id) nil)
-            (do (db/create-url id url) (db/create-url url id) id)
-            (recur (gen-short-id chars)))))))
+    (loop [short-id (utils/gen-id)]
+          (if (= (db/get-url short-id) nil)
+            (do (db/create-url short-id url) short-id)
+            (recur (gen-short-id chars)))))
 
 (defn redirect-url-service [short-id]
   (let [url (db/get-url short-id)]
-    (println url short-id)
     (if (not= url nil) url nil)))
